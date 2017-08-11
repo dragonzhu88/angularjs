932919471d56bd3f03134d9856bf7e894fcac4ef
dragon <dragonzhu1988@126.com>
2017-08-11 13:34:54 +0700

    start

---
+++ controller/con.js
@@ -0,0 +1,10 @@
+/**
+ * Created by dragon on 2017/7/28.
+ */
+angular.module('myApp', []).controller('namesCtrl', function($scope) {
+    $scope.names = [
+        {name:'Jani',country:'Norway'},
+        {name:'Hege',country:'Sweden'},
+        {name:'Kai',country:'Denmark'}
+    ];
+});
